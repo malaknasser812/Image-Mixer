be5969ab365e09d965ee757b6e4af75275d67210
fou65 <115308809+fou65@users.noreply.github.com>
2023-12-17 00:25:38 +0200

    a new ui

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from scipy.fft import fft
-import scipy.signal as sig
 import numpy as np
-import pandas as pd
-import pyqtgraph as pg
-import os
-import time
-from scipy.interpolate import interp1d
-import pyqtgraph as pg
-from matplotlib.figure import Figure
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QGraphicsScene ,QLabel , QHBoxLayout
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from PyQt5 import QtWidgets, uic 
-from matplotlib.pyplot import figure
-import matplotlib.pyplot as plt
 from cmath import*
 from numpy import *
 from Mixer import MyDialog as MX
@@ -42,7 +31,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.mousePressPosition = None
         self.mouseMovePosition = None
         #button connection
-        self.component_btn.clicked.connect(self.open_dialog)
+        #self.component_btn.clicked.connect(self.open_dialog)
         self.output_graphs = [self.output1, self.output2]
 
         image_graphs = [self.image1, self.image2, self.image3, self.image4] #this is a list of qlabel widgets
